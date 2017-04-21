e752047775993e1229759f20421240aaedb5e4f3
Varun Naik <vcnaik94@gmail.com>
2017-04-21 16:46:45 -0500

    Interactive prompt for name

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
 int main() {
-	std::cout << "Hello, world!" << std::endl;
+	std::cout << "Enter your name: ";
+	std::string name;
+	std::getline(std::cin, name);
+	std::cout << "Hello, " << name << "!" << std::endl;
 	return 0;
 }
